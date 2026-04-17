713a4f4f17a8ec255d8cd21e02aefc11605361d5
zelalemabrham638-spec <zelalemabrham638@gmail.com>
2026-04-17 13:03:21 +0300

    Final Clean Build: Brana Social

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'brana-super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///brana_final.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Models
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run()
